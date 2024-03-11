7fc48dca6b8e119b99f5b5a98aa5334541085491
vegh <vegh@spsejecna.cz>
2024-03-11 08:07:41 +0100

    added methods to Sheep.java

--- src/Animal.java
+++ src/Animal.java
@@ -14,4 +14,5 @@ public abstract class Animal {
                 ", specialCare='" + specialCare + '\'' +
                 '}';
     }
+
 }
--- src/Sheep.java
+++ src/Sheep.java
@@ -1,3 +1,11 @@
 public class Sheep extends Animal{
-
+    public Sheep() {
+        size = Size.BIG;
+        type = "mammal";
+        name = "Ivet";
+        specialCare = "Must be shaven every week";
+    }
+    public void makeSound(){
+        System.out.println("Moo");
+    }
 }
