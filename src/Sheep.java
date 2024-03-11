24a04a57a1d0fc193a76020ec02cf984e5044a09
vegh <vegh@spsejecna.cz>
2024-03-11 08:14:55 +0100

    Created a new class Rose.java

--- src/Sheep.java
+++ src/Sheep.java
@@ -8,4 +8,15 @@ public class Sheep extends Animal{
     public void makeSound(){
         System.out.println("Moo");
     }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Sheep{");
+        sb.append("type='").append(type).append('\'');
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", size=").append(size);
+        sb.append(", specialCare='").append(specialCare).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
