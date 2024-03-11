4a2b1a7edc65c8c19423e37958be3b7ace5774dd
vegh <vegh@spsejecna.cz>
2024-03-11 08:14:48 +0100

    Created a new class Rose.java

---
+++ src/Rose.java
@@ -0,0 +1,12 @@
+public class Rose extends Flower{
+
+    public Rose(String name, double price, double area, double chance) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = area;
+        this.chanceOfGrowth = chance;
+    }
+    public void watered(int liters){
+        chanceOfGrowth = liters*0.5 + chanceOfGrowth;
+    }
+}
