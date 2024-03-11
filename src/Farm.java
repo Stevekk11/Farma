e0a8dddf0776f319a0d5d08d19f269ef3cf47cd0
vegh <vegh@spsejecna.cz>
2024-03-11 08:35:43 +0100

    Added garden and methods

--- src/Farm.java
+++ src/Farm.java
@@ -2,11 +2,26 @@ import java.util.ArrayList;
 
 public class Farm {
     protected ArrayList<Animal> animals;
+    protected ArrayList<Flower> garden;
     protected final int MAX_ANIMALS = 20;
+    protected int gardenSize = 100;
 
     public Farm() {
         this.animals = new ArrayList<>();
-
+        this.garden = new ArrayList<>();
+    }
+    public void addAnimal(Animal a){
+        animals.add(a);
+    }
+    public void removeAnimal(Animal a){
+        animals.remove(a);
+    }
+    public void addPlant(Flower f){
+        garden.add(f);
+        gardenSize -= f.neededArea;
+        if (gardenSize == 0){
+            throw new RuntimeException("Garden is already full");
+        }
     }
 
     public void checkFullness(){
@@ -18,7 +33,12 @@ public class Farm {
            } else smallAnimals++;
        }
        if (bigAnimals >MAX_ANIMALS/2 || smallAnimals>MAX_ANIMALS/2){
-           throw new RuntimeException("The Fam is full");
+           throw new RuntimeException("The Farm is full");
        }
+    }
+    public void checkAnimalsOrPlants(String s){
+        switch (s){
+
+        }
     }
 }
